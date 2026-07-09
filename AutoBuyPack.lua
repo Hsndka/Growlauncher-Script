915c5a4b208be1a4d5fbbc6b9b5d135433089590
_Hsndika_2767 <sandikabs321@gmail.com>
2026-07-09 18:11:55 +0700

    Update AutoBuyPack.lua

--- AutoBuyPack.lua
+++ AutoBuyPack.lua
@@ -2,7 +2,7 @@ local buyerID = tostring(getDiscordID())
 
 local buyerList = {
     ["636196321232945152"] = "Author",
-	["562894447315124227"] = "Admin
+	["562894447315124227"] = "Admin"
 }
 
 function isWhitelisted(playerID)
