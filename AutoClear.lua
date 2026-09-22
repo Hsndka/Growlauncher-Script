35d6b51ab4f3c861c94afb394323f47db24772ef
_Hsndika_2767 <sandikabs321@gmail.com>
2026-09-22 16:45:02 +0700

    Add new buyer 'DNF' to buyerList
    Added a new buyer with ID '661504235271225364' and name 'DNF' to the buyerList.

--- AutoClear.lua
+++ AutoClear.lua
@@ -3,6 +3,7 @@ local buyerID = tostring(getDiscordID())
 local buyerList = {
     ["636196321232945152"] = "Author",
 	["562894447315124227"] = "Admin",
+	["661504235271225364"] = "DNF",
 	["1544903200023380091"] = "getukpasar",
 	["287490169366642688"] = "Steven",
 	["788035861618622495"] = "Jeqi",
